9d15ed558570e39abaeb168f6708162b8a77eca2
x2d7751347m <x2d7751347m@gmail.com>
2023-07-28 16:41:02 +0900

    create a header component

---
+++ site/src/jsMain/kotlin/org/example/landingpage/sections/MainSection.kt
@@ -0,0 +1,41 @@
+package org.example.landingpage.sections
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.id
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.silk.components.graphics.Image
+import org.example.landingpage.components.Header
+import org.example.landingpage.models.Section
+import org.example.landingpage.util.Constants.SECTION_WIDTH
+import org.example.landingpage.util.Res
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun MainSection() {
+    Box(
+        modifier = Modifier
+            .id(Section.Home.id)
+            .maxWidth(SECTION_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        MainBackground()
+        Header()
+    }
+}
+
+@Composable
+fun MainBackground() {
+    Image(
+        modifier = Modifier
+            .fillMaxSize()
+            .objectFit(ObjectFit.Cover),
+        src = Res.Image.background,
+        desc = "Background Image"
+    )
+}
---
+++ site/src/jsMain/kotlin/org/example/landingpage/util/Constants.kt
@@ -0,0 +1,13 @@
+package org.example.landingpage.util
+
+object Constants {
+    const val SECTION_WIDTH = 1920
+    const val FONT_FAMILY = "Roboto"
+}
+
+object Res {
+    object Image {
+        const val background = "background.svg"
+        const val logo = "logo.svg"
+    }
+}
