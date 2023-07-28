066eca9b3c2c0a8cf1b2a9e1ddacc75dfa2ac582
x2d7751347m <x2d7751347m@gmail.com>
2023-07-28 11:19:32 +0900

    Initial commit

---
+++ site/src/jsMain/kotlin/org/example/landingpage/pages/Index.kt
@@ -0,0 +1,11 @@
+package org.example.landingpage.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun HomePage() {
+    // TODO: Add content here
+    println("Hello World!")
+}
