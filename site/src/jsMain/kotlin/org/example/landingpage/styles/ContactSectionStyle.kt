8b68be47206d77da9de247ee7c9309015251bf7b
x2d7751347m <x2d7751347m@gmail.com>
2023-08-03 18:59:35 +0900

    Contact Section

---
+++ site/src/jsMain/kotlin/org/example/landingpage/styles/ContactSectionStyle.kt
@@ -0,0 +1,22 @@
+package org.example.landingpage.styles
+
+import com.varabyte.kobweb.compose.css.CSSTransition
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.borderColor
+import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.hover
+import org.example.landingpage.models.Theme
+import org.jetbrains.compose.web.css.ms
+
+val InputStyle by ComponentStyle {
+    base {
+        Modifier
+            .borderColor(Theme.LightGray.rgb)
+            .transition(CSSTransition(property = "border", duration = 200.ms))
+    }
+    hover {
+        Modifier
+            .borderColor(Theme.Primary.rgb)
+    }
+}
